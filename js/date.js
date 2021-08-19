e7159978af4bfaaef55665a052c5ae15bc0fc711
jake-mannino <yomannino@gmail.com>
2021-08-19 15:02:43 -0400

    yes

--- js/date.js
+++
@@ -1,9 +0,0 @@
-const timeTag = document.getElementById('time')
-
-function getTime() {
-    let date = new Date()
-    let time = date.toLocaleString().split(' ')[1].slice(0, 5)
-    timeTag.innerHTML = time
-    setTimeout(getTime, 1000);
-}
-getTime();
